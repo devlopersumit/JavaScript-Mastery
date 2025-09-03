65fac87cafcff0d397340ff8fd2e5d83e43d5343
devlopersumit <sumitsj2002@gmail.com>
2025-09-03 18:35:09 +0530

    Find Largest among 3 numbers

---
+++ Basics/largestNumber.js
@@ -0,0 +1,13 @@
+const num1 = 25;
+const num2 = 27;
+const num3 = 24;
+
+if(num1 > num2 && num1 > num3) {
+ console.log(num1, " is largest number");
+} 
+else if (num2>num1 && num2>num3) {
+    console.log(num2, " is largest number");
+}
+else {
+    console.log(num3, " is largest number");
+}
